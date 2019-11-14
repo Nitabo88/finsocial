2536b9f48f6e4ce035b332121963b53568640197
liceeset <lestrada@qvision.com.co>
2019-11-14 15:38:41 -0500

    Ajuste

--- src/main/java/co/com/red5g/finsonet/tasks/Navegar.java
+++ src/main/java/co/com/red5g/finsonet/tasks/Navegar.java
@@ -9,7 +9,6 @@ import net.serenitybdd.screenplay.waits.WaitUntil;
 import static co.com.red5g.finsonet.userinterfaces.ModulosAdministracionPage.LNK_REPORTES;
 import static co.com.red5g.finsonet.userinterfaces.ReporteVentasPage.SPN_CARGA;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isClickable;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isNotVisible;
 
 public class Navegar implements Task {
@@ -21,7 +20,6 @@ public class Navegar implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                WaitUntil.the(LNK_REPORTES,isClickable()),
                 Click.on(LNK_REPORTES),
                 WaitUntil.the(SPN_CARGA, isNotVisible()));
     }
