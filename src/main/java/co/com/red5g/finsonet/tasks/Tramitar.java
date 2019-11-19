0b920e8e2053333ad51711974520823877e444c5
liceeset <lestrada@qvision.com.co>
2019-11-19 12:14:17 -0500

    interacciones Diligenciar documentos requeridos

--- src/main/java/co/com/red5g/finsonet/tasks/Tramitar.java
+++ src/main/java/co/com/red5g/finsonet/tasks/Tramitar.java
@@ -8,11 +8,11 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.actions.SelectFromOptions;
 import net.serenitybdd.screenplay.waits.WaitUntil;
+import org.openqa.selenium.Keys;
 
 import static co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso1Page.*;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class Tramitar implements Task {
     private DocumentosRequeridos documentosRequeridos;
@@ -29,13 +29,12 @@ public class Tramitar implements Task {
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
                 Click.on(RBN_INSTRUCCIONES.of(documentosRequeridos.getStrTipoSolicitud())),
-                WaitUntil.the(LST_CIUDADES,isEnabled()).forNoMoreThan(3).seconds(),
-                SelectFromOptions.byVisibleText(documentosRequeridos.getStrCiudad()).from(LST_CIUDADES));
-        actor.attemptsTo(
-                SelectFromOptions.byVisibleText(documentosRequeridos.getStrAgenciaVinculacion()).from(LST_AGENCIA_VINCULACION));
-        actor.attemptsTo(
-                Enter.theValue(documentosRequeridos.getStrCalificacion()).into(TXT_CALIFICACION));
-        actor.attemptsTo(
+                WaitUntil.the(BTN_CIUDADES,isEnabled()).forNoMoreThan(3).seconds(),
+                Click.on(BTN_CIUDADES),
+                Enter.theValue(documentosRequeridos.getStrCiudad()).into(FILTRO_CIUDADES).thenHit(Keys.ENTER),
+                Click.on(BTN_AGENCIA_VINCULACION),
+                Click.on(FILTRO_AGENCIA_VINCULACION.of(documentosRequeridos.getStrAgenciaVinculacion())),
+                Enter.theValue(documentosRequeridos.getStrCalificacion()).into(TXT_CALIFICACION),
                 Click.on(RBN_INSTRUCCIONES.of(documentosRequeridos.getStrVinculacion()))
                 );
     }
