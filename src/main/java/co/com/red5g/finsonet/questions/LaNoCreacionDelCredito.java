03c29cb79beb00cf8ab3120663593f855645836b
liceeset <lestrada@qvision.com.co>
2019-11-25 11:20:41 -0500

    ajustes

--- src/main/java/co/com/red5g/finsonet/questions/LaNoCreacionDelCredito.java
+++ src/main/java/co/com/red5g/finsonet/questions/LaNoCreacionDelCredito.java
@@ -2,9 +2,7 @@ package co.com.red5g.finsonet.questions;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
-import net.serenitybdd.screenplay.questions.Text;
 
-import static co.com.red5g.finsonet.interacions.EnfocarPopUp.enfocarPopUp;
 import static co.com.red5g.finsonet.userinterfaces.NuevoCreditoPage.LBL_MENSAJE_FALLIDO;
 
 public class LaNoCreacionDelCredito implements Question<String> {
@@ -15,7 +13,6 @@ public class LaNoCreacionDelCredito implements Question<String> {
 
     @Override
     public String answeredBy(Actor actor) {
-        actor.attemptsTo(enfocarPopUp());
-        return Text.of(LBL_MENSAJE_FALLIDO).viewedBy(actor).asString();
+        return LBL_MENSAJE_FALLIDO.resolveFor(actor).getText();
     }
 }
