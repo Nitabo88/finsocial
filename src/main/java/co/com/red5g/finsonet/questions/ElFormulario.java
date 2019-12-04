f867e0b1f918668d583ea66b2ed14e12f27862a7
liceeset <lestrada@qvision.com.co>
2019-12-04 14:12:57 -0500

    ajustes

--- src/main/java/co/com/red5g/finsonet/questions/ElFormulario.java
+++ src/main/java/co/com/red5g/finsonet/questions/ElFormulario.java
@@ -6,14 +6,13 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static co.com.red5g.finsonet.interacions.CambiarPestana.cambiarPestana;
+import static co.com.red5g.finsonet.questions.SeleccionarFila.seleccionarFila;
 import static co.com.red5g.finsonet.tasks.Diligenciar.FECHA_SOLICITUD_CREDITO;
 import static co.com.red5g.finsonet.tasks.Ingresa.CEDULA_ACTOR;
 import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.LST_FILA_CHEQUEO_DOCUMENTOS;
 import static co.com.red5g.finsonet.userinterfaces.ModulosAdministracionPage.LNK_HOME;
 import static co.com.red5g.finsonet.userinterfaces.ModulosAdministracionPage.LNK_ORIGINACION;
-import static co.com.red5g.finsonet.utils.UtileriaFechas.masUnMinuto;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isClickable;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
 
 public class ElFormulario implements Question<Boolean> {
 
@@ -26,11 +25,8 @@ public class ElFormulario implements Question<Boolean> {
         cambiarPestana();
         actor.attemptsTo(
                 Click.on(LNK_HOME),
-                WaitUntil.the(LNK_ORIGINACION, isClickable()).forNoMoreThan(6).seconds(),
                 Click.on(LNK_ORIGINACION));
-        boolean fila = LST_FILA_CHEQUEO_DOCUMENTOS .of(actor.recall(CEDULA_ACTOR),actor.recall(FECHA_SOLICITUD_CREDITO)).resolveFor(actor).isPresent();
-        if(!fila){
-            fila = LST_FILA_CHEQUEO_DOCUMENTOS .of(actor.recall(CEDULA_ACTOR),masUnMinuto(actor.recall(FECHA_SOLICITUD_CREDITO))).resolveFor(actor).isPresent();
-        }
-        return fila;    }
+        SeleccionarFila lstFila = seleccionarFila(LST_FILA_CHEQUEO_DOCUMENTOS, actor.recall(CEDULA_ACTOR), actor.recall(FECHA_SOLICITUD_CREDITO));
+        return lstFila.answeredBy(actor).isPresent();
+    }
 }
