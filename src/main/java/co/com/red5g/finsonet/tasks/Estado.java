9b7194f7dc300cde70479f4c778bb7644c9d4182
Luis Mercado <sqa4@finsocial.co>
2019-12-09 14:48:37 -0500

    Actualización de clase Listado

--- src/main/java/co/com/red5g/finsonet/tasks/Estado.java
+++ src/main/java/co/com/red5g/finsonet/tasks/Estado.java
@@ -1,6 +1,7 @@
 package co.com.red5g.finsonet.tasks;
 
 import co.com.red5g.finsonet.models.ChequeoDocumento;
+import co.com.red5g.finsonet.questions.Credito;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
@@ -8,16 +9,17 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
+import static co.com.red5g.finsonet.tasks.Diligenciar.FECHA_SOLICITUD_CREDITO;
+import static co.com.red5g.finsonet.tasks.Ingresa.CEDULA_ACTOR;
 import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.*;
+import static co.com.red5g.finsonet.userinterfaces.MisEstadosPage.LST_FILA_DOCUMENTOS;
+import static co.com.red5g.finsonet.utils.UtileriaFechas.masUnMinuto;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isCurrentlyEnabled;
 
 public class Estado implements Task {
     private ChequeoDocumento chequeoDocumento;
 
-    public Estado(ChequeoDocumento chequeoDocumento) {
-        this.chequeoDocumento = chequeoDocumento;
-    }
 
     public static Performable diligenciar(ChequeoDocumento chequeoDocumento) { return instrumented(Estado.class, chequeoDocumento);
     }
@@ -31,6 +33,21 @@ public class Estado implements Task {
                 Click.on(LST_MODAL_CHECK1),
                 Enter.theValue("Datos incompletos").into("//*[@id=\"area\"]"),
                 Click.on(BTN_MODAL_ENVIAR));
+    }
+
+    public Estado(ChequeoDocumento chequeoDocumento) {
+        this.chequeoDocumento = chequeoDocumento;
+    }
+    public static Credito ubicarseConDocumento() {
+        return new Credito();
+    }
+
+    public Boolean answeredBy(Actor actor) {
+        boolean columna = LST_FILA_DOCUMENTOS.of(actor.recall(CEDULA_ACTOR),actor.recall(FECHA_SOLICITUD_CREDITO)).resolveFor(actor).isPresent();
+        if(!columna){
+            columna =  LST_FILA_DOCUMENTOS.of(actor.recall(CEDULA_ACTOR),masUnMinuto(actor.recall(FECHA_SOLICITUD_CREDITO))).resolveFor(actor).isPresent();
+        }
+        return columna;
     }
 }
 
--- src/test/java/co/com/red5g/finsonet/stepdefinitions/OriginacionStepDefinition.java
+++ src/test/java/co/com/red5g/finsonet/stepdefinitions/OriginacionStepDefinition.java
@@ -2,33 +2,19 @@ package co.com.red5g.finsonet.stepdefinitions;
 
 import co.com.red5g.finsonet.questions.QueElChequeoDeDocumentos;
 import co.com.red5g.finsonet.tasks.Completar;
-import co.com.red5g.finsonet.tasks.Diligencia;
+import co.com.red5g.finsonet.tasks.Estado;
 import co.com.red5g.finsonet.tasks.Realizar;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
-import net.serenitybdd.core.annotations.findby.By;
-import net.serenitybdd.core.pages.WebElementFacade;
-import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
-import net.serenitybdd.screenplay.actions.JavaScriptClick;
-
-import java.util.List;
 
 import static co.com.red5g.finsonet.models.builder.ChequeoDocumentoBuilder.con;
-import static co.com.red5g.finsonet.questions.SeleccionarColumna.FECHA;
-import static co.com.red5g.finsonet.questions.SeleccionarColumna.seleccionarColumna;
-import static co.com.red5g.finsonet.tasks.Diligenciar.FECHA_SOLICITUD_CREDITO;
-import static co.com.red5g.finsonet.tasks.Ingresa.CEDULA_ACTOR;
-import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.*;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
-import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getProxiedDriver;
 import static org.hamcrest.Matchers.containsString;
 
 public class OriginacionStepDefinition {
-    private static final String ESTADO_NO_EXITOSO = "Para poder continuar es necesario diligenciar";
 
     @Dado("^que (.*) quiere realizar el chequeo de un documento$")
     public void IngresarChequeo(String actor) {
@@ -39,7 +25,8 @@ public class OriginacionStepDefinition {
 
     @Cuando("^el asesor decline el chequeo del credito del cliente$")
     public void rechazarChequeoDocumentos() {
-
+        theActorInTheSpotlight().attemptsTo(Estado.ubicarseConDocumento(),
+                theActorInTheSpotlight().attemptsTo(Estado.diligenciar(con().motivo())));
     }
 
     @Entonces("^el asesor no deberia verlo en chequeo de documentos$")
@@ -56,13 +43,6 @@ public class OriginacionStepDefinition {
 
     @Entonces("^deberia ver el mensaje de adjuntar informacion$")
     public void verificarNoCreacionCredito() {
-        theActorInTheSpotlight().should(seeThat(QueElChequeoDeDocumentos.noSeGuardo(), containsString(ESTADO_NO_EXITOSO)));
-    }
-
-    @Cuando("^el asesor adjunta toda la informacion de el chequeo de documentos$")
-    public void adjuntarDocumentos() {
-        theActorInTheSpotlight().attemptsTo(
-                Diligencia.laInformacionDeChequeoDeDocumentos(con().centralesDeRiesgo())
-        );
+        theActorInTheSpotlight().should(seeThat(QueElChequeoDeDocumentos.noSeGuardo(), containsString("Para poder continuar es necesario diligenciar")));
     }
 }
