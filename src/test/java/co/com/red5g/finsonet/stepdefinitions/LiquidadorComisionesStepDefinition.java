998776827c4f310ba0927da05d80939b3ca53b80
liceeset <lestrada@qvision.com.co>
2019-12-13 08:59:28 -0500

    Reporte liquidacion

--- src/test/java/co/com/red5g/finsonet/stepdefinitions/LiquidadorComisionesStepDefinition.java
+++ src/test/java/co/com/red5g/finsonet/stepdefinitions/LiquidadorComisionesStepDefinition.java
@@ -1,4 +1,46 @@
 package co.com.red5g.finsonet.stepdefinitions;
 
+import co.com.red5g.finsonet.exceptions.ElDetalleNoCorrespondeException;
+import co.com.red5g.finsonet.questions.ReporteLiquidacionComisionesVentaNueva;
+import co.com.red5g.finsonet.questions.ReporteVentaNueva;
+import co.com.red5g.finsonet.tasks.ConsultarElReporte;
+import co.com.red5g.finsonet.tasks.IngresarAlReporteDeVentaNueva;
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Dado;
+import cucumber.api.java.es.Entonces;
+
+import static co.com.red5g.finsonet.exceptions.ElDetalleNoCorrespondeException.MENSAJE_REPORTE;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+
 public class LiquidadorComisionesStepDefinition {
+
+    @Dado("^que (.*) quiere consultar el reporte actual de liquidacion de comisiones$")
+    public void consultarReporteLiquidacionComisiones(String actor) {
+        theActorCalled(actor).attemptsTo(
+                ConsultarElReporte.deLiquidacionDeComisiones()
+        );
+    }
+
+    @Cuando("^el ingresa al reporte de liquidacion de comisiones - Venta Nueva del ciclo (.*)$")
+    public void elIngresaAlReporteDeLiquidacionDeComisionesVentaNuevaDelCicloCiclo(String ciclo) {
+        theActorInTheSpotlight().attemptsTo(
+                IngresarAlReporteDeVentaNueva.delCiclo(ciclo)
+        );
+    }
+
+    @Entonces("^el observara que el consolidado y el detalle del reporte de originacion liquidacion de comisiones - Venta Nueva es consistente$")
+    public void elObservaraQueElConsolidadoYElDetalleDelReporteDeOriginacionLiquidacionDeComisionesVentaNuevaEsConsistente() {
+        theActorInTheSpotlight().should(seeThat(ReporteLiquidacionComisionesVentaNueva.esCorrecto(),""));
+    }
+
+
+    @Cuando("^el ingresa al reporte de liquidacion de comisiones - Venta Liberada del ciclo (.*)$")
+    public void elIngresaAlReporteDeLiquidacionDeComisionesVentaLiberadaDelCicloCiclo(String ciclo) {
+    }
+
+    @Entonces("^el observara que el consolidado y el detalle del reporte de originacion liquidacion de comisiones - Venta Liberada es consistente$")
+    public void elObservaraQueElConsolidadoYElDetalleDelReporteDeOriginacionLiquidacionDeComisionesVentaLiberadaEsConsistente() {
+    }
 }
