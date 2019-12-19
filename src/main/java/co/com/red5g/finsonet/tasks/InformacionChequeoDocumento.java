2fc277077b2b6b0b24a62951208033106c272a22
liceeset <lestrada@qvision.com.co>
2019-12-19 12:51:54 -0500

    Ajustes escenarios

--- src/main/java/co/com/red5g/finsonet/tasks/InformacionChequeoDocumento.java
+++ src/main/java/co/com/red5g/finsonet/tasks/InformacionChequeoDocumento.java
@@ -1,21 +1,29 @@
 package co.com.red5g.finsonet.tasks;
 
+import static co.com.red5g.finsonet.questions.SeleccionarColumna.FECHA;
+import static co.com.red5g.finsonet.questions.SeleccionarColumna.seleccionarColumna;
+import static co.com.red5g.finsonet.tasks.FormularioSolicitudCredito.FECHA_SOLICITUD_CREDITO;
+import static co.com.red5g.finsonet.tasks.InformacionCredito.CEDULA_ACTOR;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_ACEPTAR;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_ACEPTAR1_POP_UP;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_ACEPTAR2_POP_UP;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_ACTIVAL;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_GUARDAR;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_PAPELERIA_ANTIGUA;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.LST_CHEQUEO_DOCUMENTOS_NOMBRE;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.LST_COLUMNAS_CHEQUEO_DOCUMENTOS;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.TXT_ACIERTA_DATACREDITO;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.TXT_PUNTAJE_CIFIN;
+
 import co.com.red5g.finsonet.interacions.Subir;
 import co.com.red5g.finsonet.models.ChequeoDocumento;
+import java.util.List;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.actions.JavaScriptClick;
-
-import java.util.List;
-
-import static co.com.red5g.finsonet.questions.SeleccionarColumna.FECHA;
-import static co.com.red5g.finsonet.questions.SeleccionarColumna.seleccionarColumna;
-import static co.com.red5g.finsonet.tasks.FormularioSolicitudCredito.FECHA_SOLICITUD_CREDITO;
-import static co.com.red5g.finsonet.tasks.InformacionCredito.CEDULA_ACTOR;
-import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.*;
 
 public class InformacionChequeoDocumento implements Task {
     private ChequeoDocumento chequeoDocumento;
@@ -30,18 +38,17 @@ public class InformacionChequeoDocumento implements Task {
         List<WebElementFacade> lstChequeoDocumentos = seleccionarColumna(LST_COLUMNAS_CHEQUEO_DOCUMENTOS, cedula, actor.recall(FECHA_SOLICITUD_CREDITO)).answeredBy(actor);
         String nombre = lstChequeoDocumentos.get(1).getText();
         actor.attemptsTo(
-                JavaScriptClick.on(LST_CHEQUEO_DOCUMENTOS_NOMBRE.of(cedula, actor.recall(FECHA), nombre)),
-                Click.on(BTN_PAPELERIA_ANTIGUA),
-                Click.on(BTN_ACEPTAR1_POP_UP),
-                Click.on(BTN_ACTIVAL),
-                Click.on(BTN_ACEPTAR2_POP_UP),
-                Click.on(BTN_ACEPTAR),
-                Enter.theValue(chequeoDocumento.getPuntajeCifin()).into(TXT_PUNTAJE_CIFIN),
-                Enter.theValue(chequeoDocumento.getAciertaDatacredito()).into(TXT_ACIERTA_DATACREDITO));
-        actor.attemptsTo(
-                Subir.losArchivos(),
-                Click.on(BTN_GUARDAR),
-                Click.on(BTN_ACEPTAR)
+            JavaScriptClick.on(LST_CHEQUEO_DOCUMENTOS_NOMBRE.of(cedula, actor.recall(FECHA), nombre)),
+            Click.on(BTN_PAPELERIA_ANTIGUA),
+            Click.on(BTN_ACEPTAR1_POP_UP),
+            Click.on(BTN_ACTIVAL),
+            Click.on(BTN_ACEPTAR2_POP_UP),
+            Click.on(BTN_ACEPTAR),
+            Enter.theValue(chequeoDocumento.getPuntajeCifin()).into(TXT_PUNTAJE_CIFIN),
+            Enter.theValue(chequeoDocumento.getAciertaDatacredito()).into(TXT_ACIERTA_DATACREDITO),
+            Subir.losArchivos(),
+            Click.on(BTN_GUARDAR),
+            Click.on(BTN_ACEPTAR)
         );
     }
 }
