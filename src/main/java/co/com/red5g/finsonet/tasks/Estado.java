b487ae597501fddaf3e0f0b00aa16a8c366fae64
liceeset <lestrada@qvision.com.co>
2020-01-09 12:46:43 -0500

    Ajuste metodo

--- src/main/java/co/com/red5g/finsonet/tasks/Estado.java
+++ src/main/java/co/com/red5g/finsonet/tasks/Estado.java
@@ -1,24 +1,19 @@
 package co.com.red5g.finsonet.tasks;
 
-import co.com.red5g.finsonet.models.ChequeoDocumento;
-import net.serenitybdd.core.pages.WebElementFacade;
+import static co.com.red5g.finsonet.questions.SeleccionarColumna.FECHA;
+import static co.com.red5g.finsonet.questions.SeleccionarColumna.seleccionarColumna;
+import static co.com.red5g.finsonet.tasks.FormularioSolicitudCredito.FECHA_SOLICITUD_CREDITO;
+import static co.com.red5g.finsonet.tasks.InformacionCredito.CEDULA_ACTOR;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_PENDIENTE;
+import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.LST_COLUMNAS_CHEQUEO_DOCUMENTOS;
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.JavaScriptClick;
 
-import java.util.List;
-
-import static co.com.red5g.finsonet.questions.SeleccionarColumna.FECHA;
-import static co.com.red5g.finsonet.questions.SeleccionarColumna.seleccionarColumna;
-import static co.com.red5g.finsonet.tasks.Diligenciar.FECHA_SOLICITUD_CREDITO;
-import static co.com.red5g.finsonet.tasks.Ingresa.CEDULA_ACTOR;
-import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.*;
-import static net.serenitybdd.screenplay.Tasks.instrumented;
-
 public class Estado implements Task {
-    private ChequeoDocumento chequeoDocumento;
-
     public static Performable ubicarseConDocumento() {
         return instrumented(Estado.class);
     }
