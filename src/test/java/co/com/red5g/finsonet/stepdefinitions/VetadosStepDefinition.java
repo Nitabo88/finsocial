e631fe0bd1d587da20824deb22c3fb81a66bc945
LicetDevco <lestradas@devco.com.co>
2020-02-15 17:40:05 -0500

    ajustes de explicacion

--- src/test/java/co/com/red5g/finsonet/stepdefinitions/VetadosStepDefinition.java
+++ src/test/java/co/com/red5g/finsonet/stepdefinitions/VetadosStepDefinition.java
@@ -1,11 +1,11 @@
 package co.com.red5g.finsonet.stepdefinitions;
 
-import co.com.red5g.finsonet.exceptions.NoSeVeElCreditoCreadoException;
-import co.com.red5g.finsonet.questions.Credito;
 import co.com.red5g.finsonet.questions.Veto;
+import co.com.red5g.finsonet.tasks.Eliminar;
 import co.com.red5g.finsonet.tasks.ModuloVetados;
 import co.com.red5g.finsonet.tasks.RealizarVeto;
 import co.com.red5g.finsonet.tasks.factories.Loguearse;
+import cucumber.api.java.Before;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
@@ -17,6 +17,11 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class VetadosStepDefinition {
     private static final String ESTADO_NO_EXITOSO = "El cliente ya tiene un veto interno.";
+
+    @Before("Vetados")
+    public void quitarMarca() {
+        theActorInTheSpotlight().attemptsTo(Eliminar.elVeto(a().unCliente()));
+    }
 
     @Dado("que el (.*) quiere aginar un veto interno")
     public void ubicarseEnVetados(String nombreActor) {
