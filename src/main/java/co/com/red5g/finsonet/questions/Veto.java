d73c0d0611e38bd9effdafa5e3ec5a634c537adf
sqa2 <sqa2@finsocial.co>
2020-02-16 16:10:07 -0500

    Cambios Finales modulo vetados

--- src/main/java/co/com/red5g/finsonet/questions/Veto.java
+++ src/main/java/co/com/red5g/finsonet/questions/Veto.java
@@ -1,7 +1,10 @@
 package co.com.red5g.finsonet.questions;
 
+import co.com.red5g.finsonet.models.Vetados;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.targets.Target;
 
 import static co.com.red5g.finsonet.questions.SeleccionarFila.seleccionarFila;
 import static co.com.red5g.finsonet.tasks.InformacionVeto.CEDULA_ACTOR;
@@ -9,13 +12,19 @@ import static co.com.red5g.finsonet.tasks.InformacionCredito.FECHA_CREDITO;
 import static co.com.red5g.finsonet.userinterfaces.VerificarVetoPage.LST_FILA_VETOS;
 
 public class Veto implements Question<Boolean> {
-    public static Veto existe() {
-        return new Veto();
+    private Vetados vetados;
+
+    public Veto(Vetados vetados) {
+        this.vetados = vetados;
+    }
+
+    public static Veto existe(Vetados vetados) {
+        return new Veto(vetados);
     }
 
     @Override
     public Boolean answeredBy(Actor actor) {
-        SeleccionarFila lstFila = seleccionarFila(LST_FILA_VETOS,actor.recall(CEDULA_ACTOR),actor.recall(FECHA_CREDITO));
-        return lstFila.answeredBy(actor).isPresent();
+        Target lstFila = LST_FILA_VETOS.of(vetados.getDocumentoVetados());
+        return lstFila.resolveFor(actor).isPresent();
     }
 }
--- src/main/java/co/com/red5g/finsonet/userinterfaces/VerificarVetoPage.java
+++ src/main/java/co/com/red5g/finsonet/userinterfaces/VerificarVetoPage.java
@@ -3,7 +3,7 @@ package co.com.red5g.finsonet.userinterfaces;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class VerificarVetoPage {
-    public static final Target LST_FILA_VETOS = Target.the("La fila de clientes vetados").locatedBy("//*[@id=\"tablaListado\"]/tr[1]/td[1]");
+    public static final Target LST_FILA_VETOS = Target.the("La fila de clientes vetados").locatedBy("//tr[@class='tableResult'][contains(.,'{0}')]");
 
     private VerificarVetoPage() {
         throw new IllegalStateException("Utility class");
