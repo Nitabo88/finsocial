b386b87273cb93576f5d0f5c7932f08e904f8433
liceeset <lestrada@qvision.com.co>
2020-02-18 11:24:23 -0500

    Ajustes para el before

--- src/main/java/co/com/red5g/finsonet/tasks/Eliminar.java
+++ src/main/java/co/com/red5g/finsonet/tasks/Eliminar.java
@@ -1,23 +1,39 @@
 package co.com.red5g.finsonet.tasks;
 
-import co.com.red5g.finsonet.tasks.factories.Loguearse;
+import static co.com.red5g.finsonet.userinterfaces.BusquedaVetadosPage.CHK_CRITERIO_BUSQUEDA;
+import static co.com.red5g.finsonet.userinterfaces.BusquedaVetadosPage.TXT_PARAMETRO_BUSQUEDA;
+import static co.com.red5g.finsonet.userinterfaces.VetadosPage.MNU_BUSQUEDA_VETADOS;
+import static co.com.red5g.finsonet.userinterfaces.VetadosPage.MNU_RIESGOS;
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+import co.com.red5g.finsonet.models.Vetados;
+import co.com.red5g.finsonet.tasks.factories.Ubicarse;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
-
-import static net.serenitybdd.screenplay.Tasks.instrumented;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.JavaScriptClick;
 
 public class Eliminar implements Task {
 
-    public Eliminar() {
+    private static final String TIPO_DOCUMENTO = "Cédula";
+    Vetados vetados;
+
+    public Eliminar(final Vetados vetados) {
+        this.vetados = vetados;
     }
 
-    public static Eliminar elVeto() {
-        return instrumented(Eliminar.class);
+    public static Eliminar elVeto(Vetados vetados) {
+        return instrumented(Eliminar.class, vetados);
     }
 
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                Loguearse.enFinsonet());
+            Ubicarse.enVetados(),
+            Click.on(MNU_RIESGOS),
+            Click.on(MNU_BUSQUEDA_VETADOS),
+            JavaScriptClick.on(CHK_CRITERIO_BUSQUEDA.of(TIPO_DOCUMENTO)),
+            Enter.theValue(vetados.getDocumentoVetados()).into(TXT_PARAMETRO_BUSQUEDA));
     }
 }
--- src/main/java/co/com/red5g/finsonet/userinterfaces/VetadosPage.java
+++ src/main/java/co/com/red5g/finsonet/userinterfaces/VetadosPage.java
@@ -13,11 +13,12 @@ public class VetadosPage extends PageObject {
     public static final Target TXT_VETADOS = Target.the("Busqueda vetados").located(By.id("txtParBusqueda"));
     public static final Target MNU_RIESGOS = Target.the("Mostrar menu vetados").located(By.id("hamburger"));
     public static final Target MNU_INTERNO = Target.the("tab veto interno").located(By.id("veto-interno"));
+    public static final Target MNU_BUSQUEDA_VETADOS = Target.the("tab veto interno").located(By.id("busq-vetados"));
     public static final Target MNU_LST_VETADOS = Target.the("tab veto lista de vetados").located(By.id("listado-vetados"));
     public static final Target TXT_DOCUMENTO_VETADOS = Target.the("input text documento").located(By.id("txtDocumento"));
     public static final Target LST_MOTIVO = Target.the(" lista de motivos vetados").located(By.id("txtMotivo"));
     public static final Target TXT_DETALLE = Target.the("detalle vetados").located(By.id("txtDetalle"));
     public static final Target BTN_NUEVO_VETO = Target.the("boton agregar veto").located(By.id("addVeto"));
     public static final Target BTN_DETALLE_VETO = Target.the("boton detalle del veto").locatedBy(("//div[@id='btnDetalle1292']"));
-    public static final Target BTN_ANULAR_VETO = Target.the("boton anular veto").locatedBy(("//button[@class=\"btn btn-green text-white\"]"));
+    public static final Target BTN_ANULAR_VETO = Target.the("boton anular veto").locatedBy(("//button[@class='btn btn-green text-white']"));
 }
