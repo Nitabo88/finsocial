d22a1c5e4992eaef4c13fbadfcc28a738d531a62
liceeset <lestrada@qvision.com.co>
2020-02-18 17:41:56 -0500

    Credio Huy, Dado

--- src/main/java/co/com/red5g/finsonet/userinterfaces/ComercialPage.java
+++ src/main/java/co/com/red5g/finsonet/userinterfaces/ComercialPage.java
@@ -4,7 +4,8 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class ComercialPage {
-    public static final Target LNK_NUEVO_CREDITO = Target.the("el link nuevo_credito").located(By.id("nuevo_credito"));
+    public static final Target LNK_NUEVO_CREDITO = Target.the("el link nuevo_credito").located(By.xpath("//h3[text()='Nuevo Crédito']//parent::a//div"));
+    public static final Target LNK_NUEVO_CREDITO_HUY= Target.the("el link nuevo_credito").located(By.xpath("//h3[text()='Nuevo Crédito Huy']//parent::a//div"));
 
     private ComercialPage() {
         throw new IllegalStateException("Utility class");
