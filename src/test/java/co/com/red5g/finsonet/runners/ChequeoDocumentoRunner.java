3c5c130b718e7f8afb3ed360802a0c92327a454c
liceeset <lestrada@qvision.com.co>
2020-02-18 20:22:57 -0500

    Ajustes Chequeo Documentos

--- src/test/java/co/com/red5g/finsonet/runners/ChequeoDocumentoRunner.java
+++ src/test/java/co/com/red5g/finsonet/runners/ChequeoDocumentoRunner.java
@@ -6,7 +6,11 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/originacion.feature", glue = "co.com.red5g.finsonet.stepdefinitions", tags = "@ChequeoDocumentos", snippets = SnippetType.CAMELCASE)
+@CucumberOptions(
+    features = "src/test/resources/features/originacion.feature",
+    glue = "co.com.red5g.finsonet.stepdefinitions",
+    tags = "@ChequeoDocumentos",
+    snippets = SnippetType.CAMELCASE)
 
 public class ChequeoDocumentoRunner {
 }
