a7032d7cb5e46900515e3fc0fd3dd6a0ab2696dc
liceeset <lestrada@qvision.com.co>
2020-02-19 11:27:16 -0500

    Ajustes de tiempos

--- src/main/java/co/com/red5g/finsonet/tasks/RealizarVeto.java
+++ src/main/java/co/com/red5g/finsonet/tasks/RealizarVeto.java
@@ -6,6 +6,7 @@ import static co.com.red5g.finsonet.userinterfaces.VetadosPage.LST_MOTIVO;
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.TXT_DETALLE;
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.TXT_DOCUMENTO_VETADOS;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 import co.com.red5g.finsonet.models.Vetados;
 import net.serenitybdd.screenplay.Actor;
@@ -14,9 +15,12 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.actions.SelectFromOptions;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 import org.openqa.selenium.Keys;
 
 public class RealizarVeto implements Task {
+
+    private static final int TIEMPO = 5;
     private Vetados vetados;
 
     public RealizarVeto(Vetados vetados) {
@@ -30,6 +34,7 @@ public class RealizarVeto implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
+            WaitUntil.the(TXT_DOCUMENTO_VETADOS, isVisible()).forNoMoreThan(TIEMPO).seconds(),
             Enter.theValue(vetados.getDocumentoVetados()).into(TXT_DOCUMENTO_VETADOS).thenHit(Keys.ENTER),
             SelectFromOptions.byVisibleText(vetados.getListaVetados()).from(LST_MOTIVO),
             Enter.theValue(vetados.getDetalleVetados()).into(TXT_DETALLE),
