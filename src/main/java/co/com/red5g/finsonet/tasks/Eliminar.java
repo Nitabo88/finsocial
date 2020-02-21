a4d1b9e76c08b110819cbe57082e6749d0b733c0
liceeset <lestrada@qvision.com.co>
2020-02-21 11:54:26 -0500

    Estabilizacion escenario

--- src/main/java/co/com/red5g/finsonet/tasks/Eliminar.java
+++ src/main/java/co/com/red5g/finsonet/tasks/Eliminar.java
@@ -9,7 +9,6 @@ import static co.com.red5g.finsonet.userinterfaces.ModulosAdministracionPage.MNU
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.MNU_LST_VETADOS;
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.MNU_RIESGOS;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isClickable;
 
 import co.com.red5g.finsonet.models.Vetados;
 import co.com.red5g.finsonet.tasks.factories.Ubicarse;
@@ -18,11 +17,10 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.JavaScriptClick;
 import net.serenitybdd.screenplay.conditions.Check;
-import net.serenitybdd.screenplay.waits.WaitUntil;
 
 public class Eliminar implements Task {
 
-    private static final String MNU_SALIR = "Salir";
+    private static final String SALIR = "Salir";
     private static final int TIEMPO = 5;
     Vetados vetados;
 
@@ -40,20 +38,18 @@ public class Eliminar implements Task {
         actor.attemptsTo(
             Ubicarse.enVetados(),
             Click.on(MNU_RIESGOS),
-            Click.on(MNU_LST_VETADOS));
-        actor.attemptsTo(
+            Click.on(MNU_LST_VETADOS),
             Check.whether(BTN_DETALLE_VETO.of(numeroDocumento).resolveFor(actor).isPresent())
                 .andIfSo(
                     JavaScriptClick.on(BTN_DETALLE_VETO.of(numeroDocumento)),
-                    WaitUntil.the(BTN_ANULAR_VETO, isClickable()).forNoMoreThan(TIEMPO).seconds(),
                     JavaScriptClick.on(BTN_ANULAR_VETO),
-                    WaitUntil.the(BTN_ANULAR, isClickable()).forNoMoreThan(TIEMPO).seconds(),
                     JavaScriptClick.on(BTN_ANULAR),
-                    WaitUntil.the(BTN_ACEPTAR, isClickable()).forNoMoreThan(TIEMPO).seconds(),
-                    JavaScriptClick.on(BTN_ACEPTAR)),
-            WaitUntil.the(BTN_MI_CUENTA, isClickable()).forNoMoreThan(TIEMPO).seconds(),
-            JavaScriptClick.on(BTN_MI_CUENTA),
-            JavaScriptClick.on(MNU_MI_CUENTA.of(MNU_SALIR))
-        );
+                    JavaScriptClick.on(BTN_ACEPTAR),
+                    JavaScriptClick.on(BTN_MI_CUENTA),
+                    JavaScriptClick.on(MNU_MI_CUENTA.of(SALIR)))
+                .otherwise(
+                    JavaScriptClick.on(BTN_MI_CUENTA),
+                    JavaScriptClick.on(MNU_MI_CUENTA.of(SALIR))
+                ));
     }
 }
--- src/main/java/co/com/red5g/finsonet/tasks/RealizarVeto.java
+++ src/main/java/co/com/red5g/finsonet/tasks/RealizarVeto.java
@@ -1,19 +1,21 @@
 package co.com.red5g.finsonet.tasks;
 
+import static co.com.red5g.finsonet.userinterfaces.ReporteVentasPage.SPN_CARGANDO;
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.BTN_ACEPTAR_VETO;
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.BTN_NUEVO_VETO;
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.LST_MOTIVO;
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.TXT_DETALLE;
 import static co.com.red5g.finsonet.userinterfaces.VetadosPage.TXT_DOCUMENTO_VETADOS;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isNotVisible;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 import co.com.red5g.finsonet.models.Vetados;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.JavaScriptClick;
 import net.serenitybdd.screenplay.actions.SelectFromOptions;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 import org.openqa.selenium.Keys;
@@ -36,10 +38,13 @@ public class RealizarVeto implements Task {
         actor.attemptsTo(
             WaitUntil.the(TXT_DOCUMENTO_VETADOS, isVisible()).forNoMoreThan(TIEMPO).seconds(),
             Enter.theValue(vetados.getDocumentoVetados()).into(TXT_DOCUMENTO_VETADOS).thenHit(Keys.ENTER),
+            WaitUntil.the(SPN_CARGANDO, isNotVisible()).forNoMoreThan(TIEMPO).seconds(),
             SelectFromOptions.byVisibleText(vetados.getListaVetados()).from(LST_MOTIVO),
+            WaitUntil.the(SPN_CARGANDO, isNotVisible()).forNoMoreThan(TIEMPO).seconds(),
             Enter.theValue(vetados.getDetalleVetados()).into(TXT_DETALLE),
-            Click.on(BTN_NUEVO_VETO),
-            Click.on(BTN_ACEPTAR_VETO)
+            WaitUntil.the(SPN_CARGANDO, isNotVisible()).forNoMoreThan(TIEMPO).seconds(),
+            JavaScriptClick.on(BTN_NUEVO_VETO),
+            JavaScriptClick.on(BTN_ACEPTAR_VETO)
         );
     }
 }
