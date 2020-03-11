919f1b2c463dd71e2937458429cfd8771e89623b
sqa2 <sqa2@finsocial.co>
2020-03-11 16:29:02 -0500

    Llamadas validacion modal 8 pasos

--- src/main/java/co/com/red5g/finsonet/tasks/FormularioSolicitudCredito.java
+++ src/main/java/co/com/red5g/finsonet/tasks/FormularioSolicitudCredito.java
@@ -1,5 +1,14 @@
 package co.com.red5g.finsonet.tasks;
 
+import co.com.red5g.finsonet.models.builders.InformacionBalancePersonalBuilder;
+import co.com.red5g.finsonet.models.builders.InformacionFinancieraBuilder;
+import co.com.red5g.finsonet.models.builders.ReferenciasBuilder;
+import co.com.red5g.finsonet.tasks.factories.Escribe;
+import co.com.red5g.finsonet.userinterfaces.*;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+
 import static co.com.red5g.finsonet.interacions.CambiarPestana.cambiarPestana;
 import static co.com.red5g.finsonet.models.builders.ActividadLaboralBuilder.es;
 import static co.com.red5g.finsonet.models.builders.DescripcionActivosBuilder.por;
@@ -7,25 +16,7 @@ import static co.com.red5g.finsonet.models.builders.DocumentoRequeridosBuilder.l
 import static co.com.red5g.finsonet.models.builders.InformacionConyugueBuilder.e;
 import static co.com.red5g.finsonet.models.builders.InformacionPersonalBuilder.cualEs;
 import static co.com.red5g.finsonet.models.builders.InformacionPrestamoBuilder.la;
-import static co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso8Page.BTN_ACEPTAR_ENCUESTA;
-import static co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso8Page.BTN_ENCUESTA;
-import static co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso8Page.BTN_ENVIAR_ENCUESTA;
-
-import co.com.red5g.finsonet.models.builders.InformacionBalancePersonalBuilder;
-import co.com.red5g.finsonet.models.builders.InformacionFinancieraBuilder;
-import co.com.red5g.finsonet.models.builders.ReferenciasBuilder;
-import co.com.red5g.finsonet.tasks.factories.Escribe;
-import co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso1Page;
-import co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso2Page;
-import co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso3Page;
-import co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso4Page;
-import co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso5Page;
-import co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso6Page;
-import co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso7Page;
-import co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso8Page;
-import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.actions.Click;
+import static co.com.red5g.finsonet.userinterfaces.FormularioSolicitudPaso8Page.*;
 
 public class FormularioSolicitudCredito implements Task {
 
@@ -35,30 +26,33 @@ public class FormularioSolicitudCredito implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-            Escribe.losDocumentosRequeridos(los().documentosSolicitante()),
-            Escribe.laInformacionDelPrestamo(la().informacionPrestamo()),
-            Click.on(FormularioSolicitudPaso1Page.BTN_GUARDAR_CONTINUAR),
-            Escribe.laInformacionPersonal(cualEs().informacionPersonal()),
-            Click.on(FormularioSolicitudPaso2Page.BTN_GUARDAR_CONTINUAR),
-            Escribe.laInformacionLaboral(es().actividadLaboral()),
-            Click.on(FormularioSolicitudPaso3Page.BTN_GUARDAR_CONTINUAR),
-            Escribe.laInformacionDelBalancePersonal(InformacionBalancePersonalBuilder.la().persona()),
-            Escribe.laInformacionDeLosActivos(por().laPersona()),
-            Escribe.elDetalleDeLaInformacionFinanciera(InformacionFinancieraBuilder.la().persona()),
-            Click.on(FormularioSolicitudPaso4Page.BTN_GUARDAR_CONTINUAR),
-            Escribe.laInformacionDelConyugue(cualEs().informacionPersonalConyugue(), e().informacionLaboralConyugue()),
-            Click.on(FormularioSolicitudPaso5Page.BTN_GUARDAR_CONTINUAR),
-            Escribe.laInformacionDeLasReferencias(
-                ReferenciasBuilder.es().informacionReferenciaFamiliar(),
-                ReferenciasBuilder.es().informacionReferenciaPersonal(),
-                ReferenciasBuilder.es().informacionReferenciaFinanciera()),
-            Click.on(FormularioSolicitudPaso6Page.BTN_GUARDAR_CONTINUAR),
-            Click.on(FormularioSolicitudPaso7Page.BTN_GUARDAR_CONTINUAR),
-            Click.on(FormularioSolicitudPaso8Page.BTN_ENVIAR),
-            Click.on(BTN_ENCUESTA.of(VISITA_COLEGIO)),
-            Click.on(BTN_ENVIAR_ENCUESTA),
-            Click.on(BTN_ACEPTAR_ENCUESTA),
-            cambiarPestana()
+                Escribe.losDocumentosRequeridos(los().documentosSolicitante()),
+                Escribe.laInformacionDelPrestamo(la().informacionPrestamo()),
+                Click.on(FormularioSolicitudPaso1Page.BTN_GUARDAR_CONTINUAR),
+                Escribe.laInformacionPersonal(cualEs().informacionPersonal()),
+                Click.on(FormularioSolicitudPaso2Page.BTN_GUARDAR_CONTINUAR),
+                Escribe.laInformacionLaboral(es().actividadLaboral()),
+                Click.on(FormularioSolicitudPaso3Page.BTN_GUARDAR_CONTINUAR),
+                Escribe.laInformacionDelBalancePersonal(InformacionBalancePersonalBuilder.la().persona()),
+                Escribe.laInformacionDeLosActivos(por().laPersona()),
+                Escribe.elDetalleDeLaInformacionFinanciera(InformacionFinancieraBuilder.la().persona()),
+                Click.on(FormularioSolicitudPaso4Page.BTN_GUARDAR_CONTINUAR),
+                Escribe.laInformacionDelConyugue(cualEs().informacionPersonalConyugue(), e().informacionLaboralConyugue()),
+                Click.on(FormularioSolicitudPaso5Page.BTN_GUARDAR_CONTINUAR),
+                Escribe.laInformacionDeLasReferencias(
+                        ReferenciasBuilder.es().informacionReferenciaFamiliar(),
+                        ReferenciasBuilder.es().informacionReferenciaPersonal(),
+                        ReferenciasBuilder.es().informacionReferenciaFinanciera()),
+                Click.on(FormularioSolicitudPaso6Page.BTN_GUARDAR_CONTINUAR),
+                Click.on(FormularioSolicitudPaso7Page.BTN_GUARDAR_CONTINUAR),
+                Click.on(FormularioSolicitudPaso8Page.BTN_ENVIAR));
+        //WaitUntil.the(BTN_ENCUESTA, isVisible()).forNoMoreThan(5).seconds(),
+        actor.attemptsTo(
+                Click.on(BTN_ENCUESTA.of(VISITA_COLEGIO)));
+        actor.attemptsTo(
+                Click.on(BTN_ENVIAR_ENCUESTA),
+                Click.on(BTN_ACEPTAR_ENCUESTA),
+                cambiarPestana()
         );
     }
 }
--- src/main/java/co/com/red5g/finsonet/userinterfaces/FormularioSolicitudPaso8Page.java
+++ src/main/java/co/com/red5g/finsonet/userinterfaces/FormularioSolicitudPaso8Page.java
@@ -8,7 +8,7 @@ public final class FormularioSolicitudPaso8Page {
     public static final Target BTN_REGRESAR = Target.the("el boton regresar").located(By.id("btnRegresar_8"));
     public static final Target BTN_ENVIAR = Target.the("el boton enviar").located(By.xpath("//button[contains(.,'Enviar')]"));
     public static final Target BTN_SUBIR_DOCUMENTOS = Target.the("el boton subir documentos").located(By.id("subOD"));
-    public static final Target BTN_ENCUESTA = Target.the("el boton de encuesta").locatedBy("//p[text()='{0}']");
+    public static final Target BTN_ENCUESTA = Target.the("el boton de encuesta").locatedBy("//p[text()='{0}']//ancestor::div[@class='grid-item']");
     public static final Target BTN_ENVIAR_ENCUESTA = Target.the("el boton enviar encuesta").locatedBy("#btn-guardar-contacto");
     public static final Target BTN_ACEPTAR_ENCUESTA = Target.the("el boton aceptar encuesta").located(By.xpath("//button[text()='Aceptar']"));
 
