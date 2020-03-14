ecdf8c8d3d3538cf004416e0bacd0c1631ff483a
liceeset <lestrada@qvision.com.co>
2020-03-13 19:30:13 -0500

    modificacion serenity  properties y ruta de scripter

--- src/main/java/co/com/red5g/finsonet/interacions/Subir.java
+++ src/main/java/co/com/red5g/finsonet/interacions/Subir.java
@@ -24,7 +24,7 @@ public class Subir implements Interaction {
         for (int i = 0; i < BTN_UPLOAD.resolveAllFor(actor).size(); i++) {
           actor.attemptsTo(Click.on(BTN_UPLOAD.resolveAllFor(actor).get(i)));
           try {
-            final String RUTA_SCRIPT = ".src/test/resources/scripts/ScriptUploadFile.exe";
+            String RUTA_SCRIPT = "./src/test/resources/scripts/ScriptUploadFile.exe";
             Runtime.getRuntime().exec(RUTA_SCRIPT);
           } catch (IOException e) {
             logger.log(Level.INFO, String.valueOf(e));
