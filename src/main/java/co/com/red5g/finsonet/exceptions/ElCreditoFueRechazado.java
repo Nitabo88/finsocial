1ab54c61d112c9fa2ae4aecc6180c1345eccf5d9
Luis Mercado <sqa4@finsocial.co>
2020-03-16 10:00:01 -0500

    No registra

--- src/main/java/co/com/red5g/finsonet/exceptions/ElCreditoFueRechazado.java
+++ src/main/java/co/com/red5g/finsonet/exceptions/ElCreditoFueRechazado.java
@@ -1,7 +1,7 @@
 package co.com.red5g.finsonet.exceptions;
 
 public class ElCreditoFueRechazado extends AssertionError {
-    public static final String MENSAJE_USUARIO_VETADO = "El cliente se encuentra vetado";
+    public static final String MENSAJE_USUARIO_VETADO = "NO REGISTRA";
 
     public ElCreditoFueRechazado(final String mensaje, final Throwable causa) {
         super(mensaje, causa);
