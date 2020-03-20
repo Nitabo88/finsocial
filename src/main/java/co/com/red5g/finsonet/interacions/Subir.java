218d4e8e2c663741b60d970e2a0e65cab1da711f
liceeset <lestrada@qvision.com.co>
2020-03-20 17:14:16 -0500

    Ajuste con javascript

--- src/main/java/co/com/red5g/finsonet/interacions/Subir.java
+++ src/main/java/co/com/red5g/finsonet/interacions/Subir.java
@@ -3,23 +3,16 @@ package co.com.red5g.finsonet.interacions;
 import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_CERRAR;
 import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.BTN_UPLOAD;
 import static co.com.red5g.finsonet.userinterfaces.ChequeoDocumentosPage.LNK_FILE_UPLOAD;
-import static co.com.red5g.finsonet.userinterfaces.ReporteVentasPage.SPN_CARGANDO;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isNotVisible;
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getProxiedDriver;
 
-import java.awt.Dialog;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
-import net.serenitybdd.screenplay.actions.EnterValue;
 import net.serenitybdd.screenplay.actions.JavaScriptClick;
 import net.serenitybdd.screenplay.actions.Upload;
-import net.serenitybdd.screenplay.waits.WaitUntil;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class Subir implements Interaction {
 
@@ -30,14 +23,14 @@ public class Subir implements Interaction {
     @Override
     public <T extends Actor> void performAs(T actor) {
         Path path = Paths.get("./src/test/resources/file/prueba.pdf");
+        ((JavascriptExecutor) getProxiedDriver()).executeScript(
+            "HTMLInputElement.prototype.click = function() { if(this.type !== 'file') HTMLElement.prototype.click.call(this); };");
         while (!BTN_UPLOAD.resolveAllFor(actor).isEmpty()) {
             for (int i = 0; i < BTN_UPLOAD.resolveAllFor(actor).size(); i++) {
                 actor.attemptsTo(
                     JavaScriptClick.on(BTN_UPLOAD.resolveAllFor(actor).get(i)),
-                    WaitUntil.the(SPN_CARGANDO, isNotVisible()).forNoMoreThan(20).seconds(),
                     Upload.theFile(path).to(LNK_FILE_UPLOAD),
-                    Click.on(BTN_CERRAR),
-                    Enter.keyValues(Keys.ESCAPE).into(BTN_UPLOAD.resolveAllFor(actor).get(i))
+                    Click.on(BTN_CERRAR)
                 );
             }
         }
