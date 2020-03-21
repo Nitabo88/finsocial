24020d3315c8d1debe54aafb1c05094853d66c82
liceeset <lestrada@qvision.com.co>
2020-03-21 00:58:22 -0500

    se agrega espera

--- src/main/java/co/com/red5g/finsonet/tasks/SeValida.java
+++ src/main/java/co/com/red5g/finsonet/tasks/SeValida.java
@@ -5,24 +5,31 @@ import static co.com.red5g.finsonet.userinterfaces.LlamadasPage.BTN_ESTADO_CDAS_
 import static co.com.red5g.finsonet.userinterfaces.LlamadasPage.TXT_BUSCAR_CDAS;
 import static co.com.red5g.finsonet.userinterfaces.ModulosAdministracionPage.IMG_FINSONET;
 import static co.com.red5g.finsonet.userinterfaces.ModulosAdministracionPage.LNK_CDAS;
+import static co.com.red5g.finsonet.userinterfaces.ReporteVentasPage.SPN_CARGANDO;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isNotVisible;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 public class SeValida implements Task {
+
+    private final static int TIEMPO = 120;
 
     public static Performable queLaLlamada() {
         return instrumented(SeValida.class);
     }
+
     @Override
     public <T extends Actor> void performAs(T actor) {
-        String numeroCredito = theActorInTheSpotlight().recall(NUMERO_CREDITO);;
+        String numeroCredito = theActorInTheSpotlight().recall(NUMERO_CREDITO);
         theActorInTheSpotlight().attemptsTo(
+            WaitUntil.the(SPN_CARGANDO, isNotVisible()).forNoMoreThan(TIEMPO).seconds(),
             Click.on(IMG_FINSONET),
             Click.on(LNK_CDAS),
             Enter.theValue(numeroCredito).into(TXT_BUSCAR_CDAS),
