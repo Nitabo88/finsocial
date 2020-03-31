3029c7862dee1eae6cd2202d7a79a84e6ebbb7b9
LicetDevco <lestradas@devco.com.co>
2020-03-30 20:16:41 -0500

    renombramiento de metodos

--- src/test/java/co/com/red5g/finsonet/stepdefinitions/FormalizacionStepDefinition.java
+++ src/test/java/co/com/red5g/finsonet/stepdefinitions/FormalizacionStepDefinition.java
@@ -1,22 +1,18 @@
 package co.com.red5g.finsonet.stepdefinitions;
 
+import static co.com.red5g.finsonet.exceptions.NoSeVeElCreditoException.MENSAJE_CREDITO;
+import static co.com.red5g.finsonet.models.builders.FormalizacionBuilder.con;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+
 import co.com.red5g.finsonet.exceptions.NoSeVeElCreditoException;
 import co.com.red5g.finsonet.questions.factories.ElCredito;
 import co.com.red5g.finsonet.tasks.factories.Consulta;
 import co.com.red5g.finsonet.tasks.factories.Diligencia;
-import co.com.red5g.finsonet.tasks.factories.Loguearse;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
-import net.serenitybdd.screenplay.actions.Click;
-
-import static co.com.red5g.finsonet.exceptions.NoSeVeElCreditoException.MENSAJE_CREDITO;
-import static co.com.red5g.finsonet.models.builders.FormalizacionBuilder.con;
-import static co.com.red5g.finsonet.userinterfaces.ModulosAdministracionPage.LNK_ORIGINACION;
-import static co.com.red5g.finsonet.userinterfaces.OriginacionPage.MNU_ORIGINACION;
-import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
-import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
-import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class FormalizacionStepDefinition {
 
@@ -51,13 +47,5 @@ public class FormalizacionStepDefinition {
   @Entonces("^el asesor debera ver el credito en el paso de tesoreria$")
   public void verificarCredito() {
     theActorInTheSpotlight().should(seeThat(ElCredito.enTesoreria()).orComplainWith(NoSeVeElCreditoException.class, MENSAJE_CREDITO));
-  }
-
-  @Dado("^que (.*) ingresa en el paso de formalizacion$")
-  public void queUnAsesorIngresaEnElPasoDeFormalizacion(String actor) {
-    theActorCalled(actor).attemptsTo(
-        Loguearse.enFinsonet(),
-        Click.on(LNK_ORIGINACION),
-        Click.on(MNU_ORIGINACION.of("Formaliza")));
   }
 }
